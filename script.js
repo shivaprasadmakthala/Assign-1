4cbb22a9012e4d28bc472edb8fd78eefad76fda1
shivaprasadmakthala <91283153+shivaprasadmakthala@users.noreply.github.com>
2022-02-16 15:12:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,125 @@
+// console.log("Welcome to guvi jjh batch ")
+// console.log(2+2)
+// var firstName = "Shiva";
+// var secondname = "Prasad";
+// console.log(firstName+" "+secondname);
+// var num1 = 2.2;
+// var num2 = 4.5;
+// console.log(num1 + num2);
+
+//Boolean DataType = conditional checking
+// true or false 
+// var isEligible = false;
+
+//undefined
+// var firstName;
+// console.log(firstName);
+
+//null
+// no value at all
+// var id = null;
+// console.log(typeof id);
+
+// let a;
+// console.log(a);
+// console.log(typeof a);
+
+// let string = "Lavish";
+// console.log(string);
+// console.log(typeof string);
+
+// let empid = 12345;
+// console.log(empid);
+// console.log(typeof empid);
+
+// let isFullStackDeveloper = true;
+// console.log(isFullStackDeveloper);
+// console.log(typeof isFullStackDeveloper);
+
+// let xyz = null;
+// let abc = "";
+// // console.log(xyz);
+// // console.log(typeof xyz);
+// if (xyz == abc) {
+//     console.log("True");
+// } else {
+//     console.log("False");
+// }
+
+// operator are the symbols / keywords that tell your js engine to perform some sort of actions
+// operand with operator 
+
+// var sum = 1 + 2;
+// console.log(sum);
+
+// 1 and 2 => operands
+// =,+ => operator
+
+// 1.ArithmeticOperator=> +, -, *, /, %
+// 2.AssignmentOperator=> RHS values assigned to left hand operand
+// 3.ComparisonOperator or RelationalOperator=> ==, ===,<,>,<=,>=,!=,!==
+// 4.LogicalOperator=> &&, ||, !
+// 5.UnaryOperator
+// 6.StringOperator
+// 7.BitWiseOperator=> &, |, <<, >>
+
+// AssignmentOperator
+// let num = 10;
+// console.log(num);
+
+// arithmeticOperators
+
+// let num1 = 20;
+// let num2 = 10;
+// console.log(`Sum:${num1 + num2}`);
+// console.log(`Sub:${num1 - num2}`);
+// console.log(`Mul:${num1 * num2}`);
+// console.log(`Div:${num1 / num2}`); //Quotient
+// console.log(`Mod:${num1 % num2}`); //Remainder
+
+// let num = 520;
+// if (num % 2 === 0) {
+//   console.log(`${num} is even`);
+// } else {
+//   console.log(`${num} is odd`);
+// }
+// Template Literal
+
+// let a = 10;
+// let b = 20;
+// let sum = 0;
+// sum = sum + (a + b);
+// sum += a + b ;
+// console.log(sum);
+
+//BODMAS = Bracket open div mul add sub 
+
+// let age = 18;
+// if (age > 15) {
+//   console.log("You are eligible to vote");
+// } else {
+//   console.log("You are not eligible to vote");
+// }
+
+// let time = 22;
+// if (time >= 0 && time < 12) {
+//   console.log("Good Morning");
+// } else if (time >= 12 && time < 16) {
+//   console.log("Good Afternoon");
+// } else if (time >= 16 && time < 20) {
+//   console.log("Good Evening");
+// } else if (time >= 20 && time < 24) {
+//   console.log("Good Night");
+// }
+
+// UnaryOperator
+// ++,--
+
+// let x = 10;
+// x = x + 1; //11
+// x += 1; //12
+// x++; //13
+// x--; //12
+// x -= 1; //11
+// console.log(x);
+
